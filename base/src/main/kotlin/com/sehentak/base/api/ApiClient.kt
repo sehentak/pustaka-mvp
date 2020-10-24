f9ea3155b531dffdfe88d0ee02d0a8fb9ff07e74
Angger Prasetyo <angger.pr@gmail.com>
2020-10-25 01:31:04 +0700

    Adding init API non SSL

--- base/src/main/kotlin/com/sehentak/base/api/ApiClient.kt
+++ base/src/main/kotlin/com/sehentak/base/api/ApiClient.kt
@@ -1,5 +1,6 @@
 package com.sehentak.base.api
 
+import android.annotation.SuppressLint
 import com.sehentak.base.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -7,7 +8,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 /**
  * Create instances using [Retrofit.Builder.build] to generate an implementation.
@@ -72,6 +79,72 @@ object ApiClient {
 
         client.readTimeout(timeOutInSeconds, TimeUnit.SECONDS)
         client.writeTimeout(timeOutInSeconds, TimeUnit.SECONDS)
+
+        if (BuildConfig.DEBUG) client.addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client.build())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(services)
+    }
+
+    /**
+     * Create an implementation of the API endpoints defined by the [services] interface.
+     * Without SSL checker or every certificate issuer will be allowed here.
+     * The relative path for a given method is obtained from an annotation on the method
+     * describing the request type.
+     *
+     * @param   services Services class that contains all request method
+     * @param   baseUrl The specified endpoint values (such as with [retrofit2.http.GET] are
+     *          resolved against this value using [okhttp3.HttpUrl]
+     */
+    fun <T> initNonCertificate(services: Class<T>, baseUrl: String): T {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts: Array<TrustManager> = arrayOf(
+            object : X509TrustManager {
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+
+                }
+
+                @SuppressLint("TrustAllX509TrustManager")
+                override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {
+
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+
+        // Install the all-trusting trust manager
+        val sslContext: SSLContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+        val client = OkHttpClient.Builder()
+        client.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        client.hostnameVerifier { _, _ -> true }
+
+        client.apply {
+            networkInterceptors().add(Interceptor { chain ->
+                val original = chain.request()
+                val request = original.newBuilder()
+                    .method(original.method, original.body)
+                    .build()
+                chain.proceed(request)
+            })
+        }
+
+        client.readTimeout(BuildConfig.ENV_TIMEOUT_REQUEST, TimeUnit.SECONDS)
+        client.writeTimeout(BuildConfig.ENV_TIMEOUT_REQUEST, TimeUnit.SECONDS)
 
         if (BuildConfig.DEBUG) client.addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
