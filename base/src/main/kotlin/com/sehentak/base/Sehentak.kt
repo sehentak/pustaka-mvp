9e9e87231e46007e7cc544a48c9a03a69a49b761
Angger Prasetyo <angger.pr@gmail.com>
2020-11-01 15:49:35 +0700

    Add function to init application context

--- base/src/main/kotlin/com/sehentak/base/Sehentak.kt
+++ base/src/main/kotlin/com/sehentak/base/Sehentak.kt
@@ -1,6 +1,7 @@
 package com.sehentak.base
 
 import android.app.Application
+import android.content.Context
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.security.NetworkSecurityPolicy
@@ -9,17 +10,25 @@ class Sehentak: Application() {
     private lateinit var instanceSecurity: NetworkSecurityPolicy
 
     companion object {
+        private lateinit var context: Context
         private var isDebug = false
 
         var isApplicationDebug: Boolean
             get() = this.isDebug
             set(value) {
-                isDebug = value
+                this.isDebug = value
+            }
+
+        var mContext: Context
+            get() = this.context
+            set(value) {
+                this.context = value
             }
     }
 
     override fun onCreate() {
         super.onCreate()
+        mContext = this
 
         if (VERSION.SDK_INT >= VERSION_CODES.N) {
             mNetworkSecurity?.isCleartextTrafficPermitted(isDebug.toString())
